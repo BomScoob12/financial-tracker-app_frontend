944f50ba6c5b485a5daa1b26cfe75a75b5775709
BomScoob12 <sorawitkraukham@gmail.com>
2024-07-26 12:50:23 +0700

    feat : tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          yellow: '#FFEAAD',
+          green: '#028f76',
+          red: '#d14334',
+        },
+        secondary: {
+          'green-light': '#B3E099',
+        },
+        catagories: {
+          food: 'yellow-400',
+        }
+      }
+    },
   },
   plugins: [
     require('daisyui'),

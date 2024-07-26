69927962211e1b176430efc69bdfb8be0e79f161
BomScoob12 <sorawitkraukham@gmail.com>
2024-07-26 12:51:29 +0700

    feat : component NewEntry.

---
+++ src/components/action/NewEntryComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MainBtn from '@/components/MainBtn.vue';
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl">Add new entry</h1>
+    <MainBtn btnColor="bg-green-600">
+      <template #btn-title>Income</template>
+    </MainBtn>
+    <MainBtn btnColor="bg-red-700">
+      <template #btn-title>Expense</template>
+    </MainBtn>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,25 +1,19 @@
 <script setup lang="ts">
-import MainBtn from '../components/MainBtn.vue';
-import DailyEntry from '../components/DailyEntry.vue';
-import DateBar from '../components/unit-component/DateBar.vue';
+import DailyEntry from '@/components/daily-component/DailyEntry.vue';
+import DateBar from '@/components/unit-component/DateBar.vue';
+import NewEntryComponent from '@/components/action/NewEntryComponent.vue';
 </script>
 
 <template>
   <div>
     <!-- Section adding new entry -->
     <section>
-      <h1 class="text-[32px]">Add new entry</h1>
-      <MainBtn btnColor="bg-green-600">
-        <template #btn-title>Income</template>
-      </MainBtn>
-      <MainBtn btnColor="bg-red-700">
-        <template #btn-title>Expense</template>
-      </MainBtn>
+      <NewEntryComponent />
     </section>
 
     <!-- Section display daily -->
     <section class="">
-      <h1 class="text-[32px]">Daily</h1>
+      <h1 class="text-3xl">Daily</h1>
       <div class="bg-[#FFEAAD] h-full p-2">
         <DateBar />
         <DailyEntry :title="'Fish'" :price="999.99" :catagory="'Food'" />
